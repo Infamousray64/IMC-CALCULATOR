56e0013a30b705997a6cdd1135415ed80310768b
infamousray64 <ramonmarronr@gmail.com>
2024-07-08 16:54:53 -0400

    feature: add barra de estatura y peso

--- app/src/main/java/com/example/imccalculator/ImcCaculatorActivity.kt
+++ app/src/main/java/com/example/imccalculator/ImcCaculatorActivity.kt
@@ -1,15 +1,89 @@
 package com.example.imccalculator
 
+import android.content.Intent
+import android.icu.text.DecimalFormat
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Range
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.google.android.material.slider.RangeSlider
 
 class ImcCaculatorActivity : AppCompatActivity() {
+    private var isMaleSelected: Boolean = true;
+    private var isFemaleSelected: Boolean = false;
+
+    private lateinit var viewMale: CardView;
+    private lateinit var viewFemale: CardView;
+    private lateinit var tvHeight: TextView;
+    private lateinit var rsHeight: RangeSlider;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_imc_caculator)
-        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc_caculator);
+        initComponent();
+        initListeners();
+        initUI();
     }
+
+    private fun initComponent() {
+        viewMale = findViewById(R.id.viewMale);
+        viewFemale = findViewById(R.id.viewFemale);
+        tvHeight = findViewById(R.id.tvHeight);
+        rsHeight = findViewById(R.id.rsHeight);
+
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener {
+            changeGender();
+            setGenderColor();
+        }
+        viewFemale.setOnClickListener {
+            changeGender();
+            setGenderColor();
+
+        }
+
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val df = DecimalFormat("#.##");
+            val result = df.format(value);
+            tvHeight.text = "$result cm";
+
+        }
+
+    }
+
+    private fun changeGender() {
+        isMaleSelected = !isMaleSelected;
+        isFemaleSelected = !isFemaleSelected;
+
+    }
+
+    private fun setGenderColor() {
+
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected));
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected));
+
+
+    }
+
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+
+        val colorReference = if (isSelectedComponent) {
+            R.color.backgorund_component_selected;
+        } else {
+            R.color.background_component;
+        }
+        return ContextCompat.getColor(this, colorReference);
+    }
+
+    private fun initUI() {
+        setGenderColor();
+
+    }
+
+}
+
+
