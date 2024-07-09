fec664387a8a10e7f45062a1b471e062836aac50
infamousray64 <ramonmarronr@gmail.com>
2024-07-09 14:42:07 -0400

    feature: add barra de estatura y botones para edad y peso

--- app/src/main/java/com/example/imccalculator/ImcCaculatorActivity.kt
+++ app/src/main/java/com/example/imccalculator/ImcCaculatorActivity.kt
@@ -8,16 +8,27 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 
 class ImcCaculatorActivity : AppCompatActivity() {
     private var isMaleSelected: Boolean = true;
     private var isFemaleSelected: Boolean = false;
+    private var currentWeight: Int = 60;
+    private var currentAge: Int = 18;
+
 
     private lateinit var viewMale: CardView;
     private lateinit var viewFemale: CardView;
     private lateinit var tvHeight: TextView;
     private lateinit var rsHeight: RangeSlider;
+    private lateinit var btnSubtractWeight: FloatingActionButton;
+    private lateinit var btnPlusWeight: FloatingActionButton;
+    private lateinit var tvWeight: TextView;
+    private lateinit var btnSubtractAge: FloatingActionButton;
+    private lateinit var btnPlusAge: FloatingActionButton;
+    private lateinit var tvAge: TextView;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
@@ -32,6 +43,13 @@ class ImcCaculatorActivity : AppCompatActivity() {
         viewFemale = findViewById(R.id.viewFemale);
         tvHeight = findViewById(R.id.tvHeight);
         rsHeight = findViewById(R.id.rsHeight);
+        btnSubtractWeight = findViewById(R.id.btnSubtracWeight);
+        btnPlusWeight = findViewById(R.id.btnPlusWeight);
+        btnSubtractAge = findViewById(R.id.btnSubtracAge);
+        btnPlusAge = findViewById(R.id.btnPlusAge);
+        tvWeight = findViewById(R.id.tvWeight);
+        tvAge = findViewById(R.id.tvAge);
+
 
     }
 
@@ -53,6 +71,29 @@ class ImcCaculatorActivity : AppCompatActivity() {
 
         }
 
+        btnPlusWeight.setOnClickListener {
+            currentWeight += 1;
+            setWeight()
+        }
+        btnSubtractWeight.setOnClickListener {
+            currentWeight -= 1;
+            setWeight()
+        }
+        btnPlusAge.setOnClickListener {
+            currentAge += 1;
+            setAge()
+        }
+        btnSubtractAge.setOnClickListener {
+            currentAge -= 1;
+            setAge()
+        }
+    }
+
+    private fun setWeight() {
+        tvWeight.text = currentWeight.toString()
+    }
+    private fun setAge() {
+        tvAge.text = currentAge.toString()
     }
 
     private fun changeGender() {
